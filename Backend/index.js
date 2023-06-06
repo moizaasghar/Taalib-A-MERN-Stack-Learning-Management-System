11aeb503bbc56c35e4a37704ffe4f7759f3ef5cb
Moiz Asghar <95476009+i201796@users.noreply.github.com>
2023-06-06 21:07:51 +0500

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
+
 // const studentRoutes = require("./route/studentRoutes");
 // const courseRoutes = require("./route/courseRoutes");
 const academicOfficerRoutes = require("./route/academicOfficerRoutes");
@@ -9,6 +11,12 @@ const academicOfficerRoutes = require("./route/academicOfficerRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 
 // connect to db
